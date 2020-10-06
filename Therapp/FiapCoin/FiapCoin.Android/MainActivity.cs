27ab510143ba2f122df861530d5901824d301e06
Gabriel <gpapa.contato@gmail.com>
2020-10-06 10:54:22 -0300

    permissions

--- Therapp/FiapCoin/FiapCoin.Android/MainActivity.cs
+++ Therapp/FiapCoin/FiapCoin.Android/MainActivity.cs
@@ -21,6 +21,8 @@ namespace THERAPP.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+
+
         }
     }
 }
--- Therapp/FiapCoin/FiapCoin/Views/CallPage.xaml.cs
+++ Therapp/FiapCoin/FiapCoin/Views/CallPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,12 +18,45 @@ namespace THERAPP.Views
         {
             InitializeComponent();
         }
+
         protected override void OnAppearing()
         {
             base.OnAppearing(); //must happen FIRST
             Device.BeginInvokeOnMainThread(() => { DisplayAlert(Global.Cliente.name+" agora é só aguardar!", "\nUm terapeuta já irá atende-lo...", "OK"); });
+            RunTimePermission();
 
         }
+
+        public async void RunTimePermission()
+        {
+            var status = PermissionStatus.Unknown;
+
+            status = await Plugin.Permissions.CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+
+            if (status != PermissionStatus.Granted)
+            {
+
+                status = await Utils.CheckPermissions(Permission.Camera);
+                await Utils.CheckPermissions(Permission.Microphone);
+
+            }
+
+            status = await Plugin.Permissions.CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Microphone);
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Utils.CheckPermissions(Permission.Microphone);
+
+            }
+
+            status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+
+            if (status != PermissionStatus.Granted)
+            {
+
+                status = await Utils.CheckPermissions(Permission.Camera);
+            }
+        }
+
         public void ConsultasClicked(object o, EventArgs e)
         {
             MessagingCenter.
